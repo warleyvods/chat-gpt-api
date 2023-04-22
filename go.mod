77c98ec11fc42816980723164293bda2fb39693e
Warley <warleyvods@gmail.com>
2023-04-22 09:44:38 -0300

    finished domains

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module chat-gpt-api
 
 go 1.18
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/j178/tiktoken-go v0.2.1
+)
+
+require github.com/sashabaranov/go-openai v1.5.8 // indirect
---
+++ internal/domain/entity/message.go
@@ -0,0 +1,48 @@
+package entity
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	tiktoken_go "github.com/j178/tiktoken-go"
+	"time"
+)
+
+type Message struct {
+	ID        string
+	Role      string
+	Content   string
+	Tokens    int
+	Model     *Model
+	CreatedAt time.Time
+}
+
+func NewMessage(role, content string, model *Model) (*Message, error) {
+	totalTokens := tiktoken_go.CountTokens(model.GetModelName(), content)
+	msg := &Message{
+		ID:        uuid.New().String(),
+		Role:      role,
+		Content:   content,
+		Model:     model,
+		Tokens:    totalTokens,
+		CreatedAt: time.Now(),
+	}
+	if err := msg.Validate(); err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
+
+func (m *Message) Validate() error {
+	if m.Role != "user" && m.Role != "system" && m.Role != "assistant" {
+		return errors.New("invalid role")
+	}
+
+	if m.CreatedAt.IsZero() {
+		return errors.New("invalid createdAt")
+	}
+	return nil
+}
+
+func (m *Message) GetQtdTokens() int {
+	return m.Tokens
+}
