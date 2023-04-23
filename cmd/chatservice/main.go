7f96678903981662e7df7a9822fcf92f877ef98c
Warley <warleyvods@gmail.com>
2023-04-23 16:21:18 -0300

    finished api for http

---
+++ cmd/chatservice/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"chat-gpt-api/configs"
+	"chat-gpt-api/internal/infra/repository"
+	"chat-gpt-api/internal/infra/web"
+	"chat-gpt-api/internal/infra/web/webserver"
+	"chat-gpt-api/internal/usecase/chatcompletion"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&multiStatements=true",
+		configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	repo := repository.NewChatRepositoryMySQL(conn)
+	client := openai.NewClient(configs.OpenAIApiKey)
+
+	chatConfig := chatcompletion.ChatCompletionConfigInputDTO{
+		Model:                configs.Model,
+		ModelMaxTokens:       configs.ModelMaxTokens,
+		Temperature:          float32(configs.Temperature),
+		TopP:                 float32(configs.TopP),
+		N:                    configs.N,
+		Stop:                 configs.Stop,
+		MaxTokens:            configs.MaxTokens,
+		InitialSystemMessage: configs.InitialChatMessage,
+	}
+
+	//chatConfigStream := chatcompletionstream.ChatCompletionConfigInputDTO{
+	//	Model:                configs.Model,
+	//	ModelMaxTokens:       configs.ModelMaxTokens,
+	//	Temperature:          float32(configs.Temperature),
+	//	TopP:                 float32(configs.TopP),
+	//	N:                    configs.N,
+	//	Stop:                 configs.Stop,
+	//	MaxTokens:            configs.MaxTokens,
+	//	InitialSystemMessage: configs.InitialChatMessage,
+	//}
+
+	usecase := chatcompletion.NewChatCompletionUseCase(repo, client)
+
+	//streamChannel := make(chan chatcompletionstream.ChatCompletionOutputDTO)
+	//usecaseStream := chatcompletionstream.NewChatCompletionUseCase(repo, client, streamChannel)
+
+	webserver := webserver.NewWebServer(":" + configs.WebServerPort)
+	webserverChatHandler := web.NewWebChatGPTHandler(*usecase, chatConfig, configs.AuthToken)
+	webserver.AddHandler("/chat", webserverChatHandler.Handle)
+	fmt.Println("Server running on port " + configs.WebServerPort)
+
+	webserver.Start()
+}
---
+++ internal/infra/web/webserver/webserver.go
@@ -0,0 +1,36 @@
+package webserver
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+type WebServer struct {
+	Router        chi.Router
+	Handlers      map[string]http.HandlerFunc
+	WebServerPort string
+}
+
+func NewWebServer(webServerPort string) *WebServer {
+	return &WebServer{
+		WebServerPort: webServerPort,
+		Router:        chi.NewRouter(),
+		Handlers:      make(map[string]http.HandlerFunc),
+	}
+}
+
+func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
+	s.Handlers[path] = handler
+}
+
+func (s *WebServer) Start() {
+	s.Router.Use(middleware.Logger)
+	for path, handler := range s.Handlers {
+		s.Router.Handle(path, handler)
+	}
+
+	if err := http.ListenAndServe(s.WebServerPort, s.Router); err != nil {
+		panic(err.Error())
+	}
+}
